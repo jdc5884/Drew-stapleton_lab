ac2ca719943ad1b9ec1c145bd217edf9e875a57a
Julia Castillo <jdc5884@uncw.edu>
2019-08-10 09:45:40 -0400

    Final Presenation Push
    End of summer push for updates

--- Stress_Splicing/Heirarchical/HeirarchicalModel.R
+++ Stress_Splicing/Heirarchical/HeirarchicalModel.R
@@ -5,6 +5,7 @@ library(stringr)
 ## Ordinal Net package ##
 library("ordinalNet")
 
+#### reading in and setting up calibrated data ####
 # MONTH 1 (2018_6 / JUNE) CALIBRATED DATA FRAME 
 calib_data_6 = na.omit(read.csv("../2018_6/calib_2018_6.csv")[,-1])
 calib_data_6$ztest1 = (calib_data_6$test1 - mean(calib_data_6$test1))/sd(calib_data_6$test1)
@@ -30,14 +31,15 @@ calib_data_11$month ='nov'
 calib_data = rbind(calib_data_6, calib_data_8, calib_data_11)
 
 # Create dummy varible columns for each month
-calib_data$june = ifelse(str_detect(calib_data[,4], "june"), 1, 0)
-calib_data$aug = ifelse(str_detect(calib_data[,4], "aug"), 1, 0)
-calib_data = calib_data[,-4]
+calib_data$june = ifelse(str_detect(calib_data[,6], "june"), 1, 0)
+calib_data$aug = ifelse(str_detect(calib_data[,6], "aug"), 1, 0)
+calib_data = calib_data[,-6]
 
 # Drop rows containing NA
-calib_data = na.omit(calib_data)
 calib_subset = calib_data[,c(1, 4:7)]
+######
 
+#### Plotting calibrated data ####
 #graphing log starting quantity to cp values
 plot(calib_data$allP,log(calib_data$startq), col = 'red', main = "Hierarchical Log Starting Quanitty vs. Cp Values", 
       ylab = "log(Starting Quantity)", xlab = "Cp Values" , xlim = c(5, 25), ylim = c(-6, 1))
@@ -75,7 +77,7 @@ abline(lm(log(calib_data_11$startq)~calib_data_11$allP), col = 'red')
 abline(lm(log(calib_data_11$startq)~calib_data_11$test1), col = 'blue')
 legend('topright', legend=c("Test 1", "All Products"),
        col=c("blue", "red"), lty = 1, cex=0.8)
-
+#####
 
 ###### POLR models ######
 # Ordinal Logistic Regression Model 
@@ -88,129 +90,101 @@ options(scipen=999)
 ## combined table
 (ctable <- cbind(ctable, "p value" = p))
 
-
-# OLRM - SQ ~ Test1
-model1 = polr(as.factor(calib_subset$startq) ~ ztest1 + june + aug, data = calib_subset, Hess = TRUE)
-#(summary(model))
-(ctable <- coef(summary(model1)))
-## calculate and store p values
-p <- pnorm(abs(ctable[, "t value"]), lower.tail = FALSE) * 2
-options(scipen=999)
-## combined table
-(ctable <- cbind(ctable, "p value" = p))
-
-# OLRM - SQ ~ allP
-model2 = polr(as.factor(calib_subset$startq) ~ zallP + june + aug, data = calib_subset, Hess = TRUE)
-#(summary(model))
-(ctable <- coef(summary(model2)))
-## calculate and store p values
-p <- pnorm(abs(ctable[, "t value"]), lower.tail = FALSE) * 2
-options(scipen=999)
-## combined table
-(ctable <- cbind(ctable, "p value" = p))
-
-
 ###### Ordinal Net models #######
 
-
-### model 1 ###
-#define ordinal model startq~ztest1+month
-ordmod1 = ordinalNet(as.matrix(calib_subset[,2:4]), as.factor(calib_subset$startq))
-summary(ordmod1)
-coef(ordmod1, matrix=TRUE)
-#kfold cv
-set.seed(123)
-ordfit1 = ordinalNetTune(as.matrix(calib_subset[,c(2:4)]), as.factor(calib_subset$startq),
-                         family = "cumulative",
-                         link = "logit", parallelTerms = TRUE, nonparallelTerms = TRUE, 
-                         warn = FALSE, printProgress = FALSE)
-head(ordfit1$loglik)
-bestLambdaIndex = which.max(rowMeans(ordfit1$loglik))
-head(coef(ordfit1$fit, matrix = TRUE, whichLambda = bestLambdaIndex))
-
-### model 2 ###
-#define ordinal model starq~zallp+month
-ordmod2 = ordinalNet(as.matrix(calib_subset[,c(2,3,5)]), as.factor(calib_subset$startq))
-summary(ordmod2)
-coef(ordmod2, matrix=TRUE)
-#kfold cv
-set.seed(123)
-ordfit2 = ordinalNetTune(as.matrix(calib_subset[,c(2,3,5)]), as.factor(calib_subset$startq),
-                         family = "cumulative",
-                         link = "logit", parallelTerms = TRUE, nonparallelTerms = TRUE, 
-                         warn = FALSE, printProgress = FALSE)
-head(ordfit2$loglik)
-bestLambdaIndex = which.max(rowMeans(ordfit2$loglik))
-head(coef(ordfit2$fit, matrix = TRUE, whichLambda = bestLambdaIndex))
-
-### model 3 ###
 #define ordinal model starq~zallP+ztest1+month
-ordmod3 = ordinalNet(as.matrix(calib_subset[,2:5]), as.factor(calib_subset$startq))
-summary(ordmod3)
-coef(ordmod3, matrix=TRUE)
+ordmod = ordinalNet(as.matrix(calib_subset[,2:5]), as.factor(calib_subset$startq))
+summary(ordmod)
+coef(ordmod, matrix=TRUE)
 #kfold cv
-set.seed(123)
-ordfit3 = ordinalNetTune(as.matrix(calib_subset[,2:5]), as.factor(calib_subset$startq), family = "cumulative",
+set.seed(3)
+ordfit = ordinalNetTune(as.matrix(calib_subset[,2:5]), as.factor(calib_subset$startq), family = "cumulative",
                          link = "logit", parallelTerms = TRUE, nonparallelTerms = TRUE, 
                          warn = FALSE, printProgress = FALSE)
-head(ordfit3$loglik)
-bestLambdaIndex = which.max(rowMeans(ordfit3$loglik))
-head(coef(ordfit3$fit, matrix = TRUE, whichLambda = bestLambdaIndex))
+head(ordfit$loglik)
+bestLambdaIndex = which.max(rowMeans(ordfit$loglik))
+head(coef(ordfit$fit, matrix = TRUE, whichLambda = bestLambdaIndex))
 
 
+###### Experimental Data #######
+
+# MONTH 1 (2018_8 / AUGUST) EXPERIMENTAL DATA FRAME
+exp_data_8 = na.omit(read.csv("../2018_8/exp_2018_8.csv")[,-1])
+exp_data_8$ztest1 = (exp_data_8$test1 - mean(exp_data_8$test1))/sd(exp_data_8$test1)
+exp_data_8$zallP = (exp_data_8$allP - mean(exp_data_8$allP))/sd(exp_data_8$allP)
+exp_data_8$month ='aug'
+exp_data_8$sampleID.exp = as.factor(exp_data_8$sampleID.exp)
+#exp_data_8
+
+# MONTH 2 (2018_6 / JUNE) EXPERIMENTAL DATA FRAME 
+exp_data_6 = na.omit(read.csv("../2018_6/exp_2018_6.csv")[,-c(1,5,6)])
+exp_data_6$ztest1 = (exp_data_6$test1 - mean(exp_data_6$test1))/sd(exp_data_6$test1)
+exp_data_6$zallP = (exp_data_6$allP - mean(exp_data_6$allP))/sd(exp_data_6$allP)
+exp_data_6$month ='june'
+#exp_data_6
+
+# MONTH 3 (2018_11 / NOVEMBER) EXPERIMENTAL DATA FRAME
+exp_data_11 = na.omit(read.csv("../2018_11/exp_2018_11.csv")[,-1])
+exp_data_11$ztest1 = (exp_data_11$test1 - mean(exp_data_11$test1))/sd(exp_data_11$test1)
+exp_data_11$zallP = (exp_data_11$allP - mean(exp_data_11$allP))/sd(exp_data_11$allP)
+exp_data_11$month ='nov'
+exp_data_11$sampleID.exp = as.factor(exp_data_11$sampleID.exp)
+#exp_data_11
+
+# Combined exp d.f. for all months
+exp_data = rbind(exp_data_6, exp_data_8, exp_data_11)
+
+# Create dummy varible columns for each month
+exp_data$june = ifelse(str_detect(exp_data[,6], "june"), 1, 0)
+exp_data$aug = ifelse(str_detect(exp_data[,6], "aug"), 1, 0)
+exp_data = exp_data[,-6]
+
+# Drop rows containing NA
+exp_subset = exp_data[,c(1, 4:7)]
+
+#### calculating experimental starting quantity ####
+probmat = predict(ordfit$fit, as.matrix(exp_subset[,2:5]))
+probmat[1:10,]
 
 
+##### finding adjustment value and adjusted test 1 in calibrated #####
 
+group = split.data.frame(calib_data, calib_data$startq)
 
-##### building separate month models into one #####
-ordmod6 = ordinalNet(as.matrix(calib_data_6[,4:5]), as.factor(calib_data_6$startq))
-summary(ordmod3)
-coef(ordmod3, matrix=TRUE)
-
-ordmod8 = ordinalNet(as.matrix(calib_data_8[,4:5]), as.factor(calib_data_6$startq))
-summary(ordmod3)
-coef(ordmod3, matrix=TRUE)
-
-ordmod11 = ordinalNet(as.matrix(calib_data_11[,4:5]), as.factor(calib_data_6$startq))
-summary(ordmod3)
-coef(ordmod3, matrix=TRUE)
-
-
-
-
-
-
-
-
-
-
-#######################################
-####### k-Fold Cross Validation ####### ordinalNet model
-#######################################
-library(caret)
-
-
-n = length(calib_subset[,1])
-set.seed(1)
-f <- 5
-folds <- rep_len(1:f, length.out = dim(calib_subset)[1])
-folds <- sample(folds, size = dim(calib_subset)[1], replace = F)
-table(folds)
-
-
-preds = NULL
-truth = NULL
-for (k in 1:f){
-  test.ID = which(folds == k)
-  train_y = calib_subset[-test.ID, "startq"]
-  train_x = calib_subset[-test.ID, 2:5]
-  test_y = calib_subset[test.ID, "startq"]
-  test_x = calib_subset[test.ID, 2:5]
-  truth = c(truth, test_y)
-  model.fit = ordinalNet(as.matrix(train_x), as.factor(train_y)) 
-  preds = c(preds, predict(model.fit, test_x)) #need to fix s.t. results in 9 values rather than 45
+adj <- function(AllP, Test1){
+  adjust = ave(AllP)-ave(Test1)
+  return(adjust)
 }
 
-confusionMatrix(preds,truth)
-ordinalNet(as.matrix(calib_subset[,2:5]), as.factor(calib_subset$startq))
+adjval = NULL
+for (k in group){
+  adjval = c(adjval,adj(k$allP, k$test1))
+}
+
+calib_data$adjval = adjval
+calib_data$adjusted_test1 = calib_data$test1 + adjval
+
+
+##### Creating a dataframe with the stq and adjustment values #########
+
+# used as a key for the adjustment per start q
+calib_adj = as.data.frame(cbind(as.numeric(as.character(unique(calib_data$startq))), unique(calib_data$adjval)))
+
+# Rename columns
+colnames(calib_adj)=c("startq", "adj")
+
+# Apply probability matrix to the adjustment values
+apply(probmat, 1, function(x) x*calib_adj$adj)
+exp_data$exp.adjust = colSums(apply(probmat, 1, function(x) x*calib_adj$adj))
+
+# Create new column with stress product (VQTL input)
+exp_data$exp.adjustTest1 = exp_data$test1.exp+exp_data$exp.adjust
+exp_data$stress = exp_data$allP.exp - exp_data$exp.adjustTest1
+
+boxplot(exp_data$allP.exp, exp_data$test1.exp, exp_data$stress, main = "Boxplot of Experimental All Products, Test 1, and Stress",
+        names =c("All Products", "Test 1", "Stress"), ylab = "Cp value", col =c("blue", "red", "green"))
+
+
+
+
 
--- Stress_Splicing/model_tutorial.R
+++ Stress_Splicing/model_tutorial.R
@@ -1,12 +1,15 @@
 ### generated test data for qpcr adjustment ###
 library('dplyr')
 library('ordinalNet')
+library('precrec')
 
 ### simulation n = 21 ###
 #create the dataset with starting quantites, all products and test 1 cp values
 sd.val = 0.2
 sd.val.test = 0.1
+set.seed(1234)
 eps = rnorm(21, 0, sd.val)
+set.seed(12345)
 eps.test= rnorm(21, 0, sd.val.test)
 stq =sort(rep(c(0.010, 0.050, 0.100, 0.500, 1.000, 0.005, 5.000), 3))
 allp = (-2.5)*log(stq)+25+eps
@@ -37,7 +40,7 @@ ordmod = ordinalNet(covmat, as.factor(data$stq))
 summary(ordmod)
 coef(ordmod, matrix=TRUE)
 #kfold cv
-set.seed(123)
+set.seed(12)
 ordfit = ordinalNetTune(covmat, as.factor(data$stq), family = "cumulative",
                          link = "logit", parallelTerms = TRUE, nonparallelTerms = TRUE, 
                          warn = FALSE, printProgress = FALSE)
@@ -46,41 +49,43 @@ bestLambdaIndex = which.max(rowMeans(ordfit$loglik))
 head(coef(ordfit$fit, matrix = TRUE, whichLambda = bestLambdaIndex))
 
 
-# ##finding ajustment value##
-# group = split.data.frame(data, data$stq)
-# 
-# adj <- function(AllP, Test1){
-#   adjust = ave(AllP)-ave(Test1)
-#   return(adjust)
-# }
-# 
-# adjval = NULL
-# for (k in group){
-#   adjval = c(adjval,adj(k$allp, k$test1))
-# }
-# 
-# data$adjval = adjval
-# data$adjusted_test1 = data$test1 + adjval
-# 
-# 
-# plot( allp,log(stq), col = 'red', main = "Log Starting Quanitty vs. Cp Values", 
-#      ylab = "log(Starting Quantity)", xlab = "Cp Values", xlim= c(10, 45), ylim = c(-6, 2))
-# points(data$adjusted_test1,log(stq), col = 'blue')
-# abline(lm(log(stq)~allp), col = "red")
-# abline(lm(log(stq)~data$adjusted_test1), col = "blue")
-# legend('topright', legend=c("Adjusted Test 1", "All Products"),
-#        col=c("blue", "red"), lty = 1, cex=0.8)
 
+
+
+#### Simulate the experimental data ####
+set.seed = (2)
+expAllp.sim = rnorm(200, mean = 24.55, sd = 4.735)
+set.seed = (3)
+expTest1.sim = rnorm(200, mean = 18.01, sd = 2.77)
+
+expsim = as.data.frame(cbind(expAllp.sim, expTest1.sim))
+expsim$ztest1 = (expsim$expTest1.sim - mean(expsim$expTest1.sim))/sd(expsim$expTest1.sim)
+expsim$zallP = (expsim$expAllp.sim - mean(expsim$expAllp.sim))/sd(expsim$expAllp.sim)
+#####
+
+#### calculating experimental starting quantity ####
+probmat = prediction(ordfit$fit, as.matrix(expsim[,3:4]))
+options(scipen=999)
+probmat[1:10,]
+
+
+####
+install.packages("ROCR")
+library("ROCR")
+performance(probmat,measure="tpr", x.measure="fpr")
 ##########################################################################################
 
 ### simulation n = 99
 
 #create the dataset with starting quantites, all products and test 1 cp values
-sd.val = 1
-sd.val.test = 0.5
-eps = rnorm(21, 0, sd.val)
-eps.test= rnorm(21, 0, sd.val.test)
-stq =sort(rep(c(0.010, 0.050, 0.100, 0.500, 1.000, 0.005, 5.000), 33))
+# grab the sd from the hierarchical model
+sd.val = 2.585
+sd.val.test = 1.75
+set.seed(10)
+eps = rnorm(63, 0, sd.val)
+set.seed(20)
+eps.test= rnorm(63, 0, sd.val.test)
+stq =sort(rep(c(0.010, 0.050, 0.100, 0.500, 1.000, 0.005, 5.000), 9))
 allp = (-2.5)*log(stq)+25+eps
 test1 = -2.7*log(stq)+23+eps.test
 
@@ -115,4 +120,8 @@ ordfit = ordinalNetTune(covmat, as.factor(data$stq), family = "cumulative",
 head(ordfit$loglik)
 bestLambdaIndex = which.max(rowMeans(ordfit$loglik))
 head(coef(ordfit$fit, matrix = TRUE, whichLambda = bestLambdaIndex))
+tail(summary(ordfit$fit, whichLambda = bestLambdaIndex))
+### the devPct drops down to 0.6339
 
+probmat = predict(ordfit$fit, as.matrix(expsim[,3:4]))
+probmat[1:10,]
